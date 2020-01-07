bed3adf9d9f420ed8e41a3f683ec6c1177d17465
Philipp Grimm <grimm.philipp@gmail.com>
2020-01-07 09:22:56 +0100

    Update wifiCreds.h

--- src/wifiCreds.h
+++ src/wifiCreds.h
@@ -1,3 +1,5 @@
 //SSID and Password of your WiFi router
 const char* ssid = "*****";
 const char* password = "*****";
+
+// after changing this file enter: 'git update-index --assume-unchanged src/wifiCreds.h' to ignore the changes and to avoid this file being tracked in history.
